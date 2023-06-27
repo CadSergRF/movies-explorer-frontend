dda3dce00dbfb8093503ac9c5c6c8b7a928f9595
Sergey Evdokimov <cadsergrf@yandex.ru>
2023-06-27 16:14:59 +0700

    feat: функциональность роута /saved-films

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../Common/Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
@@ -10,9 +10,32 @@ import { handleFilterShort, handleFilterMovies } from '../../utils/utils';
 
 const SavedMovies = ({ isLoggedIn, onBurgerOpen, isLoading, setIsLoading, savedCards, handleDeleteCard }) => {
 
-  const [queriedMoviesCards, setQueriedMoviesCards] = useState([]); // стейт карточек по запросу
   const [shortsMoviesCards, setShortsMoviesCards] = useState([]); // стейт с фильтром Shorts
   const [isNotFound, setIsNotFound] = useState(false); // ничего не найдено
+  const [savedQuery, setSavedQuery] = useState(''); // запрос на время сессии
+  const [isShortMovies, setIsShortMovies] = useState(false); // чекбокс поиска
+
+  function handleSearchMovies(query) {
+    setSavedQuery(query);
+  }
+
+  function handleShortMovies() {
+    setIsShortMovies(!isShortMovies);
+  }
+
+  useEffect(() => {
+    const cardsList = handleFilterMovies(savedCards, savedQuery);
+    console.log(cardsList);
+    setShortsMoviesCards(isShortMovies ? handleFilterShort(cardsList) : cardsList);
+  }, [savedCards, isShortMovies, savedQuery]);
+
+  useEffect(() => {
+    if (shortsMoviesCards.length === 0) {
+      setIsNotFound(true);
+    } else {
+      setIsNotFound(false);
+    }
+  }, [shortsMoviesCards]);
 
   return (
     <>
@@ -21,12 +44,16 @@ const SavedMovies = ({ isLoggedIn, onBurgerOpen, isLoading, setIsLoading, savedC
         onBurgerOpen={onBurgerOpen}
       />
       <main>
-        <SearchForm />
+        <SearchForm
+          onSearchMovies={handleSearchMovies}
+          onFilter={handleShortMovies}
+        />
         <MoviesCardList
           isCardsFromSaved={true}
-          cards={savedCards}
+          cards={shortsMoviesCards}
           isLoading={isLoading}
           setIsLoading={setIsLoading}
+          isNotFound={isNotFound}
           handleDeleteCard={handleDeleteCard}
         />
       </main>
